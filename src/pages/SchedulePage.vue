9938617ea256659fd115dd3705a5d8b9451369aa
LeraDolgova109 <Dol0914@yandex.ru>
2023-03-01 23:31:19 +0700

    Реализовано поле для таймслотов

--- src/pages/SchedulePage.vue
+++ src/pages/SchedulePage.vue
@@ -2,27 +2,39 @@
   <div class="schedule">
     <div class="calendar">Календарь</div>
 
-    <div class="scheduleBlock">
-      <div class="dateName">
-        <div class="date"></div>
-        <div class="date"></div>
-        <div class="date"></div>
-        <div class="date"></div>
-        <div class="date"></div>
-        <div class="date"></div>
-      </div>
-    <div class="help">
-      <div class="timeName">
-        <div class="time"></div>
-        <div class="time"></div>
-        <div class="time"></div>
-        <div class="time"></div>
-        <div class="time"></div>
-        <div class="time"></div>
-        <div class="time"></div>
-      </div>
-      <div class="timeSlots">
+    <div class="schedule-block">
+      <div class="date-name">
+        <div class="date-block">
+          <div class="schedule_date-title">
+            <span>пн</span><span> 20 мар.</span>
+          </div></div>
+        <div class="date-block">
+          <div class="schedule_date-title">
+            <span>вт</span><span> 21 мар.</span>
+          </div></div>
+        <div class="date-block">
+          <div class="schedule_date-title">
+            <span>ср</span><span> 22 мар.</span>
+          </div></div>
+        <div class="date-block">
+          <div class="schedule_date-title">
+            <span>чт</span><span> 23 мар.</span>
+          </div></div>
+        <div class="date-block">
+          <div class="schedule_date-title">
+            <span>пт</span><span> 24 мар.</span>
+          </div></div>
+        <div class="date-block">
+          <div class="schedule_date-title">
+            <span>сб</span><span> 25 мар.</span>
+          </div></div>
+        </div>
+
+      <div class="time-slots">
         <div class="container">
+          <div class="time-block">
+          <span>8:45</span><span> 10:20</span>
+          </div>
           <div class="block block1 block2"></div>
           <div class="block block1"></div>
           <div class="block block1"></div>
@@ -31,6 +43,9 @@
           <div class="block block1 block3"></div>
         </div>
         <div class="container">
+          <div class="time-block">
+          <span>10:35</span><span> 12:10</span>
+        </div>
           <div class="block block2"></div>
           <div class="block"></div>
           <div class="block"></div>
@@ -39,6 +54,9 @@
           <div class="block block3"></div>
         </div>
         <div class="container">
+          <div class="time-block">
+          <span>12:25</span><span> 14:00</span>
+        </div>
           <div class="block block2"></div>
           <div class="block"></div>
           <div class="block"></div>
@@ -47,6 +65,9 @@
           <div class="block block3"></div>
         </div>
         <div class="container">
+          <div class="time-block">
+          <span>14:45</span><span> 16:20</span>
+        </div>
           <div class="block block2"></div>
           <div class="block"></div>
           <div class="block"></div>
@@ -55,6 +76,9 @@
           <div class="block block3"></div>
         </div>
         <div class="container">
+          <div class="time-block">
+          <span>16:35</span><span> 18:10</span>
+        </div>
           <div class="block block2"></div>
           <div class="block"></div>
           <div class="block"></div>
@@ -63,6 +87,9 @@
           <div class="block block3"></div>
         </div>
         <div class="container">
+          <div class="time-block">
+          <span>18:25</span><span> 20:00</span>
+        </div>
           <div class="block block2"></div>
           <div class="block"></div>
           <div class="block"></div>
@@ -71,6 +98,9 @@
           <div class="block block3"></div>
         </div>
         <div class="container">
+          <div class="time-block">
+          <span>20:15</span><span> 21:50</span>
+        </div>
           <div class="block block2 block4"></div>
           <div class="block block4"></div>
           <div class="block block4"></div>
@@ -78,8 +108,7 @@
           <div class="block block4"></div>
           <div class="block block3 block4"></div>
         </div>
-      </div>
-      </div>
+        </div>
     </div>
   </div>
 </template>
@@ -122,75 +151,84 @@ export default {
 
   color: rgba(241, 233, 233, 0.8);
 }
-.scheduleBlock{
+.schedule-block{
+  width:100%;
+  height: 100%;
   margin-top: 10px;
-  height: 840px;
-  left: 66px;
-  top: 136px;
-
+  padding: 5px;
   background: rgba(90, 62, 202, 0.6);
   border-radius: 15px;
 }
-.help{
+.date-name{
+  margin-left: 60px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  width: 100%;
 }
-.timeSlots {
+.date-block {
+  box-sizing: border-box;
+  position: relative;
+  width: 16%;
+  height: 35px;
+}
+span {
+  display: block;
+  padding-left: 10px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  white-space: pre;
+  color: #F1E9E9;
+}
+
+.time-block {
+  box-sizing: border-box;
+  position: relative;
+  width: 60px;
+  min-height: 115px;
+}
+.time-slots {
   display: inline-block;
+  width: 100%;
 }
 .container {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  width: 100%;
+  min-height: 115px;
+
 }
 .block {
   box-sizing: border-box;
+  display: flex;
   position: relative;
-  width: 200px;
-  height: 115px;
+  width: 15%;
+  
+  min-width: 192px;
+  min-height: 115px;
 
   border: 1px solid #E1DFED;
+}
+
+.block:hover {
+  -webkit-box-shadow: 0 0 20px #E1DFED;
+  -moz-box-shadow: 0 0 20px #E1DFED;
+  box-shadow: 0 0 20px #E1DFED;
 }
 .block1 {
   border-top: none;
 }
-
 .block2 {
   border-left: none;
 }
-
 .block3 {
   border-right: none;
 }
 .block4 {
   border-bottom: none;
-}
-.dateName{
-  margin-left: 20px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-
-.timeName{
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-}
-
-.date {
-  box-sizing: border-box;
-  position: relative;
-  width: 200px;
-  height: 20px;
-
-}
-.time {
-  box-sizing: border-box;
-  position: relative;
-  width: 20px;
-  height: 115px;
-
 }
 </style>
