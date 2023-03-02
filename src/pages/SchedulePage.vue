b80f6fd59a3c90e98bec69fef8b4a0e1f1de8173
LeraDolgova109 <Dol0914@yandex.ru>
2023-03-02 23:28:04 +0700

    Добавлен раздел Календарь, изменены стили элементов

--- src/pages/SchedulePage.vue
+++ src/pages/SchedulePage.vue
@@ -1,39 +1,53 @@
 <template>
   <div class="schedule">
-    <div class="calendar">Календарь</div>
+    <div class="calendar">
+      <div class="btn btn_left"></div>
+      <div style="margin-top: -4px; display: flex;">
+        <span style="font-weight: 400; font-size: 20px; color: rgba(241, 233, 233, 0.8);">февраль 13, 2023</span>
+        <span style="margin-left: 5px; margin-right: 5px;"> - </span>
+        <span style="font-weight: 400; font-size: 20px; color: rgba(241, 233, 233, 0.8);">февраль 18, 2023</span>
+      </div>
+      <div class="btn btn_right"></div>
+    </div>
 
     <div class="schedule-block">
       <div class="date-name">
         <div class="date-block">
           <div class="schedule_date-title">
-            <span>пн</span><span> 20 мар.</span>
+            <span style="font-weight: 600;">пн</span>
+            <span> 13 фев.</span>
           </div></div>
         <div class="date-block">
           <div class="schedule_date-title">
-            <span>вт</span><span> 21 мар.</span>
+            <span style="font-weight: 600;">вт</span>
+            <span> 14 фев.</span>
           </div></div>
         <div class="date-block">
           <div class="schedule_date-title">
-            <span>ср</span><span> 22 мар.</span>
+            <span style="font-weight: 600;">ср</span>
+            <span> 15 фев.</span>
           </div></div>
         <div class="date-block">
           <div class="schedule_date-title">
-            <span>чт</span><span> 23 мар.</span>
+            <span style="font-weight: 600;">чт</span>
+            <span> 16 фев.</span>
           </div></div>
         <div class="date-block">
           <div class="schedule_date-title">
-            <span>пт</span><span> 24 мар.</span>
+            <span style="font-weight: 600;">пт</span>
+            <span> 17 фев.</span>
           </div></div>
         <div class="date-block">
           <div class="schedule_date-title">
-            <span>сб</span><span> 25 мар.</span>
+            <span style="font-weight: 600;">сб</span>
+            <span> 18 фев.</span>
           </div></div>
         </div>
 
       <div class="time-slots">
         <div class="container">
           <div class="time-block">
-          <span>8:45</span><span> 10:20</span>
+          <span style="font-weight: 600;">8:45</span><span> 10:20</span>
           </div>
           <div class="block block1 block2"></div>
           <div class="block block1"></div>
@@ -44,7 +58,7 @@
         </div>
         <div class="container">
           <div class="time-block">
-          <span>10:35</span><span> 12:10</span>
+          <span style="font-weight: 600;">10:35</span><span> 12:10</span>
         </div>
           <div class="block block2"></div>
           <div class="block"></div>
@@ -55,7 +69,7 @@
         </div>
         <div class="container">
           <div class="time-block">
-          <span>12:25</span><span> 14:00</span>
+          <span style="font-weight: 600;">12:25</span><span> 14:00</span>
         </div>
           <div class="block block2"></div>
           <div class="block"></div>
@@ -66,7 +80,7 @@
         </div>
         <div class="container">
           <div class="time-block">
-          <span>14:45</span><span> 16:20</span>
+          <span style="font-weight: 600;">14:45</span><span> 16:20</span>
         </div>
           <div class="block block2"></div>
           <div class="block"></div>
@@ -77,7 +91,7 @@
         </div>
         <div class="container">
           <div class="time-block">
-          <span>16:35</span><span> 18:10</span>
+          <span style="font-weight: 600;">16:35</span><span> 18:10</span>
         </div>
           <div class="block block2"></div>
           <div class="block"></div>
@@ -88,7 +102,7 @@
         </div>
         <div class="container">
           <div class="time-block">
-          <span>18:25</span><span> 20:00</span>
+          <span style="font-weight: 600;">18:25</span><span> 20:00</span>
         </div>
           <div class="block block2"></div>
           <div class="block"></div>
@@ -99,7 +113,7 @@
         </div>
         <div class="container">
           <div class="time-block">
-          <span>20:15</span><span> 21:50</span>
+          <span style="font-weight: 600;">20:15</span><span> 21:50</span>
         </div>
           <div class="block block2 block4"></div>
           <div class="block block4"></div>
@@ -135,26 +149,15 @@ export default {
 <style scoped>
 
 .calendar{
-  width:100%;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
-  color:aliceblue;
-  height: 24px;
-  left: 533px;
-  top: 95px;
-
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 24px;
-
-  color: rgba(241, 233, 233, 0.8);
 }
 .schedule-block{
   width:100%;
   height: 100%;
-  margin-top: 10px;
+  margin-top: 15px;
   padding: 5px;
   background: rgba(90, 62, 202, 0.6);
   border-radius: 15px;
@@ -164,22 +167,20 @@ export default {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  width: 100%;
+  justify-content: center;
 }
 .date-block {
   box-sizing: border-box;
   position: relative;
-  width: 16%;
+  width: 16.5%;
   height: 35px;
 }
 span {
   display: block;
-  padding-left: 10px;
   font-family: 'Inter';
   font-style: normal;
-  font-weight: 600;
+  font-weight: 400;
   font-size: 14px;
-  line-height: 17px;
   white-space: pre;
   color: #F1E9E9;
 }
@@ -205,6 +206,7 @@ span {
 .block {
   box-sizing: border-box;
   display: flex;
+  flex-direction: column;
   position: relative;
   width: 15%;
   
@@ -215,9 +217,9 @@ span {
 }
 
 .block:hover {
-  -webkit-box-shadow: 0 0 20px #E1DFED;
-  -moz-box-shadow: 0 0 20px #E1DFED;
-  box-shadow: 0 0 20px #E1DFED;
+  -webkit-box-shadow: 0 0 10px #E1DFED inset;
+  -moz-box-shadow: 0 0 10px #E1DFED inset;
+  box-shadow: 0 0 10px #E1DFED inset;
 }
 .block1 {
   border-top: none;
@@ -230,5 +232,25 @@ span {
 }
 .block4 {
   border-bottom: none;
+}
+.btn {
+  width: 15px;
+  height: 15px;
+  border-top: 2px solid rgba(186, 246, 159, 0.95) !important;
+  border-right: 2px solid rgba(186, 246, 159, 0.95) !important;
+  display: inline-block;
+  transition: 1s;
+}
+.btn_left{
+  transform: rotate(-135deg);
+  margin-right: 5px;
+}
+.btn_right{
+  transform: rotate(45deg);
+  margin-left: 5px;
+}
+.btn:hover{
+  border-top: 2px solid rgba(90, 62, 202, 0.6) !important;
+  border-right: 2px solid rgba(90, 62, 202, 0.6) !important;
 }
 </style>
