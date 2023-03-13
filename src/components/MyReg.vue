7f6de4e75b92f6fc3ee3a1a5ae06aea3a7f1f314
LeraDolgova109 <Dol0914@yandex.ru>
2023-03-14 03:20:15 +0700

    Добавлен запрос для регистрации

--- src/components/MyReg.vue
+++ src/components/MyReg.vue
@@ -7,22 +7,19 @@
                <button class="button" @click="changeCard">Войти</button>
                <my-button style="width: 136px; height: 30px; margin-left: 30px; border-radius: 15px;" >Регистрация</my-button>
                 </div>
-               <input type="text" id="e-mail" placeholder="e-mail">
-               <input type="text" id="pass" placeholder="Пароль">
+                <input v-model="user.name" type="text" id="text" placeholder="Имя">
+               <input v-model="user.email" type="text" id="e-mail" placeholder="e-mail">
+               <input v-model="user.password" type="text" id="pass" placeholder="Пароль">
                <div style="margin-left: 75px;">
                 <my-select
                     v-model="selectedGroup"
                     :options="optionsGroup"
                     :name="group"
                 />
-                <my-select
-                    v-model="selectedTeacher"
-                    :options="optionsTeacher"
-                    :name="teacher"
-                />
                 </div>
-                <my-button style="margin-top: 10px; width: 446px; border-radius: 15px;" @click="postChanges">Зарегистрироваться</my-button>
-                </slot>
+                <my-button style="margin-top: 10px; width: 446px; border-radius: 15px;" @click="reg">Зарегистрироваться</my-button>
+                <span v-if="sent">{{ spanText }}</span>  
+              </slot>
         </div>
     </div>
 </template>
@@ -35,12 +32,16 @@ export default {
   name: 'my-reg',
   data(){
     return{
+        user: {
+          name: '',
+          email: '',
+          password: '',
+        },
         selectedGroup: '',
-        selectedTeacher: '',
         optionsGroup: [],
-        optionsTeacher: [],
         group: "Группа",
-        teacher: "Преподаватель",
+        spanText: 'Успешно зарегестрирован',
+        sent: false
     }
   },
   props: {
@@ -59,22 +60,44 @@ export default {
           console.log(e);
         };
       },
-      async get_teachers(){
-        try{
-          const response = await axios.get("http://185.46.8.41/api/teacher");
-          
-          this.optionsTeacher = response.data;
-        } catch (e){
-          console.log(e);
-        };
-      },
       changeCard(){
             this.$router.push('/login');
+        },
+      async reg(){
+        let group_id = '';
+        for (let i=0; i < this.optionsGroup.length; i++){
+          if (this.optionsGroup[i].name == this.selectedGroup){
+            group_id = this.optionsGroup[i].id;
+            break;
+          }
         }
+        
+        if (this.user.email == '' != this.user.name == '' || this.user.password == '' || group_id == ''){
+          this.sent = true;
+          this.spanText = "Каждое поле должно быть заполнено"
+          return;
+        }
+          try{
+              const response = await axios.post("http://185.46.8.41/api/auth/register", {
+                    "name": this.user.name,
+                    "email": this.user.email,
+                    "password": this.user.password,
+                    "group_id": group_id
+              });
+              localStorage.setItem("token", response.data.token);
+              this.sent = true;
+              this.spanText = "Успешно зарегестрирован";
+          }catch(e){
+              this.sent = true;
+              console.log(e);
+              if(e.response.request.status == 422){
+                  this.spanText = "Некорректный email либо email уже занят"
+              }
+          }
+      }
     },
     beforeMount(){
         this.get_groups();
-        this.get_teachers();
     }
 }
 </script>
@@ -159,6 +182,22 @@ input{
 
 .button:hover{
     color: rgb(255, 255, 255);
+}
+
+span{
+    margin-top: 10px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    display: table; 
+    margin: 0 auto;
+    color: #Ffff; 
+  }
+span:hover{
+  color:#F1E9E9; 
+  opacity: 0.8;
 }
 
 </style>
